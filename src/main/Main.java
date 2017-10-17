89fd33a19919c8c3a301263da8339258d46c4165
Rostislav Klein <rostislav.klein@dhl.com>
2017-10-17 09:56:01 +0200

    Textfield and event handler added.

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,12 +10,11 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -35,19 +34,42 @@ public class Main extends Application {
         BorderPane borderPane = new BorderPane();
         
         
-        Text centralText = new Text();
+        TextArea centralText = new TextArea();
         centralText.setText(hra.vratUvitani());
+        centralText.setEditable(false);
+        
         borderPane.setCenter(centralText);
         
-        Label zadejPrikaz = new Label("Zadej prikaz: ");
-        zadejPrikaz.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+        Label zadejPrikazLabel = new Label("Zadej prikaz: ");
+        zadejPrikazLabel.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+        
+        TextField zadejPrikazTextField = new TextField();
+        zadejPrikazTextField.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                
+                String vstupniPrikaz = zadejPrikazTextField.getText();
+                String odpovedHry = hra.zpracujPrikaz(vstupniPrikaz);
+                
+                centralText.appendText("\n" + vstupniPrikaz + "\n");
+                centralText.appendText("\n" + odpovedHry + "\n");
+                
+                zadejPrikazTextField.setText("");
+                
+                if(hra.konecHry()){
+                    zadejPrikazTextField.setEditable(false);
+                    centralText.appendText(hra.vratEpilog());
+                }
+                
+            }
+        });
         
         FlowPane dolniLista = new FlowPane();
         dolniLista.setAlignment(Pos.CENTER);
-        dolniLista.getChildren().add(zadejPrikaz);
+        dolniLista.getChildren().addAll(zadejPrikazLabel, zadejPrikazTextField);
         borderPane.setBottom(dolniLista);
         
-        Scene scene = new Scene(borderPane, 300, 250);
+        Scene scene = new Scene(borderPane, 700, 450);
 
         primaryStage.setTitle("Adventura");
 
